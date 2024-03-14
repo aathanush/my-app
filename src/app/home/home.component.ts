2348cff5a23dc554c14e61939fafd5247e0db23a
Thanush A  A <aathanush@gmail.com>
2024-03-14 13:57:25 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DoctorComponent } from '../doctor/doctor.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    DoctorComponent
   ],
   template: `
   <section>
     <form>
-      <input type="text" placeholder="Write doctor name">
+      <input type="text" placeholder="Filter by city">
       <button class="primary" type="button">Search</button>
     </form>
   </section>
-`,
+  <section class="results">
+    <app-doctor></app-doctor>
+  </section>
+  `,
   styleUrls: ['./home.component.css'],
 })
 
