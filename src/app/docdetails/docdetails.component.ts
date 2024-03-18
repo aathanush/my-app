1ef6190041e8522dc5134998556511ebd2a35c73
Thanush A  A <aathanush@gmail.com>
2024-03-18 17:13:00 +0530

    Update docdetails.component.ts
    to display details of doctor when clicked

--- src/app/docdetails/docdetails.component.ts
+++ src/app/docdetails/docdetails.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { PatientService } from '../patient.service';
+import { Doctor } from '../doctor';
 @Component({
   selector: 'app-docdetails',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      docdetails works!
-    </p>
-  `,
+  <article>
+    <img class="listing-photo" [src]="doctor?.photo"
+      alt="Photo of {{doctor?.docname}}"/>
+    <section class="listing-description">
+      <h2 class="listing-heading">{{doctor?.specialization}}</h2>
+      <p class="listing-location">{{doctor?.rating}}</p>
+    </section>
+  </article>
+`,
   styleUrl: './docdetails.component.css'
 })
 export class DocdetailsComponent {
-
+    route: ActivatedRoute = inject(ActivatedRoute);
+    doctor: Doctor | undefined;
+    doctorId = -1;
+    constructor() {
+        this.doctorId = Number(this.route.snapshot.params['id']);
+    }
 }
